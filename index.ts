859c7fe515f5283ea9d409a23b73ca9a80129769
Cassiano <capiap@hotmail.com>
2025-12-12 13:46:52 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+function greet(nome: string) {
+  return `Eu sou o ${nome}`;
+}
+
+console.log(greet('Cassiano'));
+// const soma = (x: number, y: number) =>  {
+// //   return  x + y;
+// }
+// console.log(soma(3, 4));
